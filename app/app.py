d7765888e6d6c8a9c2343ced65b876690487549a
Dunkel <55016616+SimonGyrin@users.noreply.github.com>
2023-01-25 20:36:34 +0300

    Flask input example

--- app/app.py
+++ app/app.py
@@ -1,3 +1,21 @@
+<<<<<<< HEAD
+from flask import Flask, render_template, url_for, request
+import pymongo
+import random
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('main.html', value=random.random())
+
+@app.route('/name_sumbit', methods=['POST', 'GET'])
+def name_sumbit():
+    a = request.form['name']
+    return render_template('name_sumbit.html', name=a)
+
+
+if __name__ == "__main__":
+=======
 from flask import Flask, render_template, url_for
 import pymongo
 app = Flask(__name__)
@@ -8,4 +26,5 @@ def home():
 
 
 if __name__ == "__main__":
+>>>>>>> 465d9a129140fe0110168e4abae78fb3a3d433c0
     app.run(debug=True)
