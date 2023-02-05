20b1bb5a7896fcd645c03cd6c7125fe3b1d6b0b7
Dunkel <55016616+SimonGyrin@users.noreply.github.com>
2023-02-05 23:42:34 +0300

    Authorization page with jQuery added
    http://127.0.0.1:5000/authorization

--- app/app.py
+++ app/app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    #a = request.form['name']
     return render_template('main.html', value=random.random())
 
-@app.route('/name_sumbit', methods=['POST', 'GET'])
+@app.route('/authorization', methods=['POST', 'GET'])
 def name_sumbit():
-    a = request.form['name']
-    return render_template('name_sumbit.html', name=a)
 
+    return render_template('authorization.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
