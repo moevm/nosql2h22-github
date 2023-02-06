0843e2e802e291bded3cffaaf402388cb12df48b
Dunkel <55016616+SimonGyrin@users.noreply.github.com>
2023-02-06 19:09:51 +0300

    menu initialization alpha v.0.0.1

--- app/app.py
+++ app/app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 import pymongo
 import random
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    #a = request.form['name']
-    return render_template('main.html', value=random.random())
+
+    return redirect('authorization')
 
 @app.route('/authorization', methods=['POST', 'GET'])
-def name_sumbit():
+def authorization():
 
     return render_template('authorization.html')
+
+@app.route('/menu', methods=['POST', 'GET'])
+def statistic():
+
+    return render_template('menu.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
