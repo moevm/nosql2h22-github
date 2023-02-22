50eb1c6adef70aa9f11eae0d0ee887f41160818b
Dunkel <55016616+SimonGyrin@users.noreply.github.com>
2023-02-22 23:54:32 +0300

    menu v0.2

--- app/static/script/menu.js
+++ app/static/script/menu.js
@@ -1,5 +1,25 @@
-$('.menu-icon').on('click', function(e) {
-    e.preventDefault();
-    $('.menu').toggleClass('menu_active');
-    $('section').toggleClass('section_active');
-  })
+
+var main = function() { //главная функция
+  
+  $('.open-menu').hover(
+    function(){
+      $('.logo').attr('src', '../static/images/252311.png');
+    },
+    function(){
+      $('.logo').attr('src', '../static/images/25231.png');
+    });
+
+  $('.open-menu').click(
+    function() {
+      $('.menu').animate({left: '0px'}, 200);
+      $('body').animate({left: '285px'}, 200);
+  });
+ 
+  $('.close-menu').click(
+    function() {
+      $('.menu').animate({left: '-285px'}, 200);
+      $('body').animate({left: '0px'}, 200);
+    });
+};
+
+$(document).ready(main);
