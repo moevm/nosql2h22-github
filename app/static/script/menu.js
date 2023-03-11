fb02acd43d58868abe2ee8a5ae4430c254498ac0
Dunkel <55016616+SimonGyrin@users.noreply.github.com>
2023-03-11 21:59:34 +0300

    menu v0.5
    Добавлено выпадающее меню для выбора параметра просмотра статистики репо;
    Баг-фикс с выдвигающимся меню.

--- app/static/script/menu.js
+++ app/static/script/menu.js
@@ -1,11 +1,30 @@
+var main = function() {
+  $('.ch-issues').click(
+    function(){
+      $('.issues').attr('style', 'display: block');
+      $('.pull-requests').attr('style', 'display: none');
+      $('.commits').attr('style', 'display: none');
+    });
 
+    $('.ch-pr').click(
+      function(){
+        $('.issues').attr('style', 'display: none');
+        $('.pull-requests').attr('style', 'display: block');
+        $('.commits').attr('style', 'display: none');
+      });
+      
+      $('.ch-commits').click(
+        function(){
+          $('.issues').attr('style', 'display: none');
+          $('.pull-requests').attr('style', 'display: none');
+          $('.commits').attr('style', 'display: block');
+        });
 
-var main = function() { //главная функция
   $('.open-menu').click(
     function() {
       $('.menu').animate({left: '0px'}, 200);
       $('body').animate({left: '285px'}, 200);
-  });
+    });
  
   $('.close-menu').click(
     function() {
