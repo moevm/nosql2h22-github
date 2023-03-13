64ec2b34a940483e43a533ad42c8506510d2d0a1
Dunkel <55016616+SimonGyrin@users.noreply.github.com>
2023-03-13 06:20:40 +0300

    authorization v0.5

--- app/static/script/authorization.js
+++ app/static/script/authorization.js
@@ -1,4 +1,7 @@
-
+var main = function(){
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
+}
+$(document).ready(main);
+$(document).ready(main_2);
